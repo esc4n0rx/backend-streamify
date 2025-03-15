ebc00ab095991fa1d61ee26fdc49f53749163e94
Paulo Oliveira <paulo.cunha@hortifruti.com.br>
2025-03-15 13:33:44 -0300

    Ajustando cors

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import favoriteRoutes from './routes/favoriteRoutes.js';
 import contentRoutes from './routes/contentRoutes.js';
 import contentAdminRoutes from './routes/contentAdminRoutes.js';
 import proxyRoutes from './routes/proxyRoutes.js';
+import cors from 'cors';
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://streamifyx.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 
 app.use(express.json());
